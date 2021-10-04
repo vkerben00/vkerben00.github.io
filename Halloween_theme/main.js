120ec0dcecdfee6f24bd42ed866328c587bbc60b
vanvan24 <66397102+vanvan24@users.noreply.github.com>
2021-10-04 03:04:23 -0400

    edits

--- Halloween_theme/main.js
+++ Halloween_theme/main.js
@@ -1,4 +1,4 @@
-blueEyesb.onclick = function() {
+/* blueEyesb.onclick = function() {
       let start = Date.now();
 
       let timer = setInterval(function() {
@@ -9,4 +9,18 @@ blueEyesb.onclick = function() {
         if (timePassed > 2000) clearInterval(timer);
 
       }, 20);
-    }
+    } */
+
+
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+let txt = "";
+for (let x in person) {
+  txt += person[x] + " ";
+};
+
+document.getElementById("demo").innerHTML = txt;
