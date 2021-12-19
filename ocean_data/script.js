f8e32365768436608588051e69b37465fbc001f1
vanvan24 <66397102+vanvan24@users.noreply.github.com>
2021-12-19 01:58:20 -0500

    edits

--- ocean_data/script.js
+++ ocean_data/script.js
@@ -1,25 +1,18 @@
 let map = L.map("mapid", {
-  center: [43.00,  -75.00], // latitude, longitude in decimal degrees (find it on Google Maps with a right click!)
-  zoom: 5, // can be 0-22, higher is closer
+  center: [-33.918861,  18.423300], // latitude, longitude in decimal degrees (find it on Google Maps with a right click!)
+  zoom: 7, // can be 0-22, higher is closer
   scrollWheelZoom: false // don't zoom the map on scroll
 });
-// add the basemap tiles
-L.tileLayer(
-  "https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg" // stamen toner tiles
-).addTo(map);
+ /* Control panel to display map layers */
+  var controlLayers = L.control.layers( null, null, {
+    position: "topright",
+    collapsed: false
+  }).addTo(map);
 
-var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  subdomains: 'abcd',
-  minZoom: 1,
-  maxZoom: 16,
-  ext: 'jpg'
-});
 
-// add zoom control in topright corner
-    //L.control.zoom({ position:'topright' }).addTo(map);
 
-    // Set Geocoder options
+
+ // Set Geocoder options
     var geocodingOptions = {
       url: 'https://api.geocode.earth/v1',
       expanded: true,
@@ -30,4 +23,45 @@ var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/w
     L.control.geocoder('ge-fc050a4033212256', geocodingOptions).addTo(map);
 
 
+
+
+  // display Carto basemap tiles with light features and labels
+  var light = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+  }).addTo(map); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
+  controlLayers.addBaseLayer(light, 'Main Map');
+
+  /* Stamen colored terrain basemap tiles with labels */
+  var terrain = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+  }); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
+  controlLayers.addBaseLayer(terrain, 'Second Version Map');
+
+  // see more basemap options at https://leaflet-extras.github.io/leaflet-providers/preview/
+
+  // Read markers data from data.csv
+  $.get('sharksightings.xlsx', function(csvString) {
+
+    // Use PapaParse to convert string to array of objects
+    var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+
+    // For each row in data, create a marker and add it to the map
+    // For each row, columns `Latitude`, `Longitude`, and `Title` are required
+    for (var i in data) {
+      var row = data[i];
+
+      var marker = L.marker([row.Latitude, row.Longitude], {
+        opacity: 1,
+        // Customize your icon
+  icon: L.icon({
+    iconUrl: 'shark.svg',
+    iconSize: [50, 100]
+  })
+      }).bindPopup(row.Date);
+       
+      
+      marker.addTo(map);
+    }
+
+  });
 
