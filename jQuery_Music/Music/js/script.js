a324fa5bc59cd1c0e8b4991f045683f6112ddbff
vanvan24 <66397102+vanvan24@users.noreply.github.com>
2021-12-18 20:43:20 -0500

    edits

--- jQuery_Music/Music/js/script.js
+++ jQuery_Music/Music/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     
   function getMusic(){
  var url='https://blmak.github.io/BigMusicClubConclomerateDates.json'
@@ -10,6 +10,41 @@ $(document).ready(function() {
     }
 
     getMusic();
-})
+}) */
 
+function printArtist(venue){
+    for (var i = 0; i < venueItems.length; i++) {
+      if (venueItems[i].venue == venue){
+        console.log(venueItems[i])
+      }
 
+    }
+  }
+  function showData(artist){
+    $(artist).toggle()
+
+  }
+  function displayArtist(venue){
+    var htmlText = ""
+    for (var i = 0; i < venueItems.length; i++) {
+
+      if (venueItems[i].venue == venue){
+        htmlText = 
+
+        '<h1 class="artist">'+venueItems[i].artist+'</h1>'+
+        '<button class="showButton" onclick="showData(item'+i+')"> Show me show time </button>'+
+        '<div class="artistData" id="item'+i+'" style="display:none">'+
+        '<p>'+venueItems[i].date+'</p>'+
+        '<p>'+venueItems[i].city+'</p>'+
+        '<p>'+venueItems[i].state+'</p>'+
+        '<p>'+venueItems[i].show_time+'</p>'+
+        '<p>$'+venueItems[i].price+'</p>'+
+        '<p>'+venueItems[i].currency+'</p>'+
+        '</div>'
+
+    $("#data").append(htmlText)
+      }
+
+    }
+    
+  }
