87878e7f57b46c1d40657825675779ea49bbd6f8
vanvan24 <66397102+vanvan24@users.noreply.github.com>
2022-12-07 23:54:45 -0500

    edits

--- website2/script.js
+++ website2/script.js
@@ -1,11 +1,22 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+const CANVAS_WIDTH = innerWidth;
+const CANVAS_HEIGHT = innerHeight;
+let gameSpeed = 2;
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+
+const playerImage = new Image();
+playerImage.src = 'spriteStandRight.png';
+const spriteWidth = 177;
+const spiteHeight = 400;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -17,6 +28,8 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
+
+
 
 class Layer {
 	constructor(image, speedModifier){
@@ -50,7 +63,7 @@ const layer1 = new Layer(backgroundLayer1, 0.2);
 const layer2 = new Layer(backgroundLayer2, 0.4);
 const layer3 = new Layer(backgroundLayer3, 0.6);
 const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
+const layer5 = new Layer(backgroundLayer5, .01);
 
 const gameObjects = [layer1,layer2,layer3,layer4,layer5];
 
@@ -61,7 +74,14 @@ function animate(){
      gameObjects.forEach(object =>{
      	object.update();
      	object.draw();
+     	ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spiteHeight, spriteWidth, spiteHeight, 0, 0, spriteWidth/2, spiteHeight/2);
      });
+
+    if(gameFrame % staggerFrames === 0){ 
+     if(frameX <59) frameX++
+     	else frameX = 0;
+ }
+    gameFrame++;
 	requestAnimationFrame(animate);
 };
 animate();
