3e7e4ba63624c2f76a6cbadb00e30ae27c05e8e1
vanvan24 <66397102+vanvan24@users.noreply.github.com>
2022-12-09 01:09:00 -0500

    edits

--- website2/script.js
+++ website2/script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
 	const ctx = canvas.getContext('2d');
 	canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    let gameSpeed = 5;
+    let gameSpeed = .5;
 
 
     //Background 
@@ -46,11 +46,11 @@ class Layer {
 	}
 }
 
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
+const layer1 = new Layer(backgroundLayer1, 1);
+const layer2 = new Layer(backgroundLayer2, 4);
+const layer3 = new Layer(backgroundLayer3, 3);
+const layer4 = new Layer(backgroundLayer4, 2);
+const layer5 = new Layer(backgroundLayer5, 0);
 
 const gameObjects = [layer1,layer2,layer3,layer4,layer5];
 
@@ -96,8 +96,6 @@ const gameObjects = [layer1,layer2,layer3,layer4,layer5];
     		this.weight = 1; 
 
     		}
-
-
 
 
       draw(context){
